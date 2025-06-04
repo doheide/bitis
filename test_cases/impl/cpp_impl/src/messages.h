c6fdd71b74dd90d1b3f5058b53197e416d17ee25
Dominik Heide <dheide@gptracks.com>
2025-06-05 00:17:01 +0200

    Added test case automation for rust and cpp

--- test_cases/impl/cpp_impl/src/messages.h
+++ test_cases/impl/cpp_impl/src/messages.h
@@ -3,41 +3,85 @@
 
 
 
-// ****** MsgSimpleBaseOneInt *****
+// ****** MsgSimpleTestBase *****
 
 
-struct MsgSimpleBaseOneInt {
+struct MsgSimpleTestBase {
     static const char *msg_attr[];
     typedef IntgralWithGivenBitSize<uint16_t, 11> Param1_T;
+    typedef BitisBool Param2_T;
+    typedef IntgralWithGivenBitSize<int8_t, 5> Param3_T;
 
     typedef message_helper::MessageT<
-        Param1_T
+        Param1_T, Param2_T, Param3_T
     > MsgT;
 
     Param1_T param_1;
+    Param2_T param_2;
+    Param3_T param_3;
 
     std::size_t serialize(BitisSerializer &ser) const {
         return message_helper::msg_serialize(this, ser);
     }
-    static bitis_helper::BitiaDeserializerHelper<MsgSimpleBaseOneInt> deserialize(BitisDeserializer &des) {
-        return message_helper::msg_deserialize<MsgSimpleBaseOneInt>(des);
+    static bitis_helper::BitiaDeserializerHelper<MsgSimpleTestBase> deserialize(BitisDeserializer &des) {
+        return message_helper::msg_deserialize<MsgSimpleTestBase>(des);
     }
 
     void print(int16_t indent=0) {
-        printf("MsgSimpleBaseOneInt{ ");
+        printf("MsgSimpleTestBase{ ");
         if (indent>=0) printf("\n");
         message_helper::msg_print(this, (indent>=0) ? (2 + indent) : indent, msg_attr);
         print_indent(indent); printf("}");
         // if (indent>=0) printf("\n");
     }
 
-    bool is_equal(const MsgSimpleBaseOneInt &other) const {
-        return param_1==other.param_1;
+    bool is_equal(const MsgSimpleTestBase &other) const {
+        return param_1==other.param_1 && param_2==other.param_2 && param_3==other.param_3;
    }
-    bool operator==(const MsgSimpleBaseOneInt &other) const { return is_equal(other); }
-    bool operator!=(const MsgSimpleBaseOneInt &other) const { return !is_equal(other); }
+    bool operator==(const MsgSimpleTestBase &other) const { return is_equal(other); }
+    bool operator!=(const MsgSimpleTestBase &other) const { return !is_equal(other); }
 };
-const char *MsgSimpleBaseOneInt::msg_attr[] = {"param_1"};
+const char *MsgSimpleTestBase::msg_attr[] = {"param_1", "param_2", "param_3"};
+
+// ****** MsgSimpleBaseThreeInt *****
+
+
+struct MsgSimpleBaseThreeInt {
+    static const char *msg_attr[];
+    typedef IntgralWithGivenBitSize<uint16_t, 11> Param1_T;
+    typedef IntgralWithGivenBitSize<uint8_t, 6> Param2_T;
+    typedef IntgralWithGivenBitSize<uint16_t, 11> Param3_T;
+
+    typedef message_helper::MessageT<
+        Param1_T, Param2_T, Param3_T
+    > MsgT;
+
+    Param1_T param_1;
+    Param2_T param_2;
+    Param3_T param_3;
+
+    std::size_t serialize(BitisSerializer &ser) const {
+        return message_helper::msg_serialize(this, ser);
+    }
+    static bitis_helper::BitiaDeserializerHelper<MsgSimpleBaseThreeInt> deserialize(BitisDeserializer &des) {
+        return message_helper::msg_deserialize<MsgSimpleBaseThreeInt>(des);
+    }
+
+    void print(int16_t indent=0) {
+        printf("MsgSimpleBaseThreeInt{ ");
+        if (indent>=0) printf("\n");
+        message_helper::msg_print(this, (indent>=0) ? (2 + indent) : indent, msg_attr);
+        print_indent(indent); printf("}");
+        // if (indent>=0) printf("\n");
+    }
+
+    bool is_equal(const MsgSimpleBaseThreeInt &other) const {
+        return param_1==other.param_1 && param_2==other.param_2 && param_3==other.param_3;
+   }
+    bool operator==(const MsgSimpleBaseThreeInt &other) const { return is_equal(other); }
+    bool operator!=(const MsgSimpleBaseThreeInt &other) const { return !is_equal(other); }
+};
+const char *MsgSimpleBaseThreeInt::msg_attr[] = {"param_1", "param_2", "param_3"};
 
 // ****** MsgSimpleTestFP *****
 
@@ -119,82 +163,38 @@ struct MsgSimpleOpt {
 };
 const char *MsgSimpleOpt::msg_attr[] = {"param_1", "param_2", "param_3", "param_4"};
 
-// ****** MsgSimpleTestBase *****
+// ****** MsgSimpleBaseOneInt *****
 
 
-struct MsgSimpleTestBase {
+struct MsgSimpleBaseOneInt {
     static const char *msg_attr[];
     typedef IntgralWithGivenBitSize<uint16_t, 11> Param1_T;
-    typedef BitisBool Param2_T;
-    typedef IntgralWithGivenBitSize<int8_t, 5> Param3_T;
 
     typedef message_helper::MessageT<
-        Param1_T, Param2_T, Param3_T
+        Param1_T
     > MsgT;
 
     Param1_T param_1;
-    Param2_T param_2;
-    Param3_T param_3;
 
     std::size_t serialize(BitisSerializer &ser) const {
         return message_helper::msg_serialize(this, ser);
     }
-    static bitis_helper::BitiaDeserializerHelper<MsgSimpleTestBase> deserialize(BitisDeserializer &des) {
-        return message_helper::msg_deserialize<MsgSimpleTestBase>(des);
+    static bitis_helper::BitiaDeserializerHelper<MsgSimpleBaseOneInt> deserialize(BitisDeserializer &des) {
+        return message_helper::msg_deserialize<MsgSimpleBaseOneInt>(des);
     }
 
     void print(int16_t indent=0) {
-        printf("MsgSimpleTestBase{ ");
+        printf("MsgSimpleBaseOneInt{ ");
         if (indent>=0) printf("\n");
         message_helper::msg_print(this, (indent>=0) ? (2 + indent) : indent, msg_attr);
         print_indent(indent); printf("}");
         // if (indent>=0) printf("\n");
     }
 
-    bool is_equal(const MsgSimpleTestBase &other) const {
-        return param_1==other.param_1 && param_2==other.param_2 && param_3==other.param_3;
+    bool is_equal(const MsgSimpleBaseOneInt &other) const {
+        return param_1==other.param_1;
    }
-    bool operator==(const MsgSimpleTestBase &other) const { return is_equal(other); }
-    bool operator!=(const MsgSimpleTestBase &other) const { return !is_equal(other); }
+    bool operator==(const MsgSimpleBaseOneInt &other) const { return is_equal(other); }
+    bool operator!=(const MsgSimpleBaseOneInt &other) const { return !is_equal(other); }
 };
-const char *MsgSimpleTestBase::msg_attr[] = {"param_1", "param_2", "param_3"};
-
-// ****** MsgSimpleBaseThreeInt *****
-
-
-struct MsgSimpleBaseThreeInt {
-    static const char *msg_attr[];
-    typedef IntgralWithGivenBitSize<uint16_t, 11> Param1_T;
-    typedef IntgralWithGivenBitSize<uint8_t, 6> Param2_T;
-    typedef IntgralWithGivenBitSize<uint16_t, 11> Param3_T;
-
-    typedef message_helper::MessageT<
-        Param1_T, Param2_T, Param3_T
-    > MsgT;
-
-    Param1_T param_1;
-    Param2_T param_2;
-    Param3_T param_3;
-
-    std::size_t serialize(BitisSerializer &ser) const {
-        return message_helper::msg_serialize(this, ser);
-    }
-    static bitis_helper::BitiaDeserializerHelper<MsgSimpleBaseThreeInt> deserialize(BitisDeserializer &des) {
-        return message_helper::msg_deserialize<MsgSimpleBaseThreeInt>(des);
-    }
-
-    void print(int16_t indent=0) {
-        printf("MsgSimpleBaseThreeInt{ ");
-        if (indent>=0) printf("\n");
-        message_helper::msg_print(this, (indent>=0) ? (2 + indent) : indent, msg_attr);
-        print_indent(indent); printf("}");
-        // if (indent>=0) printf("\n");
-    }
-
-    bool is_equal(const MsgSimpleBaseThreeInt &other) const {
-        return param_1==other.param_1 && param_2==other.param_2 && param_3==other.param_3;
-   }
-    bool operator==(const MsgSimpleBaseThreeInt &other) const { return is_equal(other); }
-    bool operator!=(const MsgSimpleBaseThreeInt &other) const { return !is_equal(other); }
-};
-const char *MsgSimpleBaseThreeInt::msg_attr[] = {"param_1", "param_2", "param_3"};
+const char *MsgSimpleBaseOneInt::msg_attr[] = {"param_1"};
--- test_cases/simple_msg/build.sh
+++ test_cases/simple_msg/build.sh
@@ -1,8 +1,71 @@
 #!/bin/bash
+set -e
 
+# compile messages to code
 ../../target/debug/bitis -d -i simple_msg.bitis compile -o ../impl/rust_impl/src/messages.rs -l rust
 
-
-
 ../../target/debug/bitis -d -i simple_msg.bitis compile -o ../impl/cpp_impl/src/messages.h -l cpp
+
+# ****
+echo -e "\n******\nrust"
+src_path=../impl/rust_impl/src/
+main_file=main.rs
+if [[ -e $src_path/$main_file ]]; then
+    echo "$main_file already exists"
+    test -L $src_path/$main_file || (>&2 echo "$src_path/$main_file has to be a symbolic link "; exit 1 )
+    rm -v $src_path/$main_file
+fi
+
+cpath=$(pwd)
+cd ../impl/rust_impl/src
+ln -s "$cpath/main.rs" main.rs
+cargo build
+cd -
+
+# ****
+echo -e "\n******\ncpp"
+src_path=../impl/cpp_impl/src/
+main_file=main.cpp
+if [[ -e $src_path/$main_file ]]; then
+    echo "$main_file already exists"
+    test -L $src_path/$main_file || (>&2 echo "$src_path/$main_file has to be a symbolic link "; exit 1 )
+    rm -v $src_path/$main_file
+fi
+
+cpath=$(pwd)
+cd ../impl/cpp_impl/src
+ln -s "$cpath/main.cpp" main.cpp
+cd ..
+
+rm -rf build
+cmake -S . -B build
+cmake --build build
+
+cd $cpath
+
+# ****
+rm -f *.dat
+
+# ****
+../impl/rust_impl/target/debug/rust_impl
+
+../impl/cpp_impl/build/test_cpp
+
+# ****
+../impl/rust_impl/target/debug/rust_impl rs
+rs_rs_err=$?
+../impl/rust_impl/target/debug/rust_impl cpp
+rs_cpp_err=$?
+../impl/cpp_impl/build/test_cpp cpp
+cpp_cpp_err=$?
+../impl/cpp_impl/build/test_cpp rs
+cpp_rs_err=$?
+
+echo -e "\nrs_rs_err: $rs_rs_err, rs_cpp_err: $rs_cpp_err, cpp_cpp_err: $cpp_cpp_err, cpp_rs_err: $cpp_rs_err"
+
+
+
+
+
+
 
